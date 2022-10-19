38eceb67d95e3dab1d8f01aa67d4e00b53a0ddf0
nemanja899 <nemanja899>
2022-10-19 17:42:33 +0000

    deleting duplicates

--- Databricks-GCP-QuickStart-Labs (1)/Databricks-GCP-QuickStart-Labs (1)/Agenda.py
+++
@@ -1,8 +0,0 @@
-# Databricks notebook source
-# MAGIC %md
-# MAGIC ## Agenda
-# MAGIC <a href = "$./1 - Introduction to Databricks Cloud Platform">1. Introduction to Databricks Lakehouse Platform</a><BR>
-# MAGIC <a href = "$./2- Getting started with Databricks Notebooks">2. Getting started with Databricks Notebooks</a><BR>
-# MAGIC <a href = "$./3 - Connecting to Google Cloud Storage">3. Connecting to Google Cloud Storage</a><BR>
-# MAGIC <a href = "$./4 - Building a Lakehouse with Delta Lake">4. Building a Lakehouse with Delta Lake</a><BR>
-# MAGIC <a href = "$./5 - Architecture Design Patterns with Databricks on GCP">5. Architecture Design Patterns with Databricks on GCP</a><BR>
